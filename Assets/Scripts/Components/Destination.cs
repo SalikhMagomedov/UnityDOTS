d36984313048eb2235490026eb781783e40da1e0
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-01-30 22:33:50 +0300

    Creating Components

---
+++ Assets/Scripts/Components/Destination.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Components
+{
+    [GenerateAuthoringComponent]
+    public struct Destination : IComponentData
+    {
+        public float3 value;
+    }
+}
