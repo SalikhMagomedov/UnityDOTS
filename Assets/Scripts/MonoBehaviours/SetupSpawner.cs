efd6ec081b3b5b7ee1df17088d96cf09dd667db2
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-01-31 22:16:58 +0300

    Spawning Multiple Entities

--- Assets/Scripts/MonoBehaviours/SetupSpawner.cs
+++ Assets/Scripts/MonoBehaviours/SetupSpawner.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Components;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MonoBehaviours
 {
@@ -9,16 +12,26 @@ namespace MonoBehaviours
         [SerializeField] private GameObject personPrefab;
         [SerializeField] private int gridSize;
         [SerializeField] private int spread;
+        [SerializeField] private Vector2 speedRange = new Vector2(4f, 7f);
 
         private BlobAssetStore _blob;
-        
+
         private void Start()
         {
             _blob = new BlobAssetStore();
             var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, _blob);
             var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(personPrefab, settings);
             var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-            entityManager.Instantiate(entity);
+            for (var x = 0; x < gridSize; x++)
+            for (var z = 0; z < gridSize; z++)
+            {
+                var instance = entityManager.Instantiate(entity);
+                var position = new float3(x * spread, 0, z * spread);
+                entityManager.SetComponentData(instance, new Translation { Value = position });
+                entityManager.SetComponentData(instance, new Destination { value = position });
+                var speed = Random.Range(speedRange.x, speedRange.y);
+                entityManager.SetComponentData(instance, new MovementSpeed { value = speed });
+            }
         }
 
         private void OnDestroy()
