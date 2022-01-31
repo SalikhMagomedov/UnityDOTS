a4396605a26fdd495f807352b0a899b605a52780
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-01-31 22:55:28 +0300

    Generating New Destinations

---
+++ Assets/Scripts/Systems/NewDestinationSystem.cs
@@ -0,0 +1,33 @@
+﻿using Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Systems
+{
+    public class NewDestinationSystem : SystemBase
+    {
+        private RandomSystem _randomSystem;
+
+        protected override void OnCreate()
+        {
+            _randomSystem = World.GetExistingSystem<RandomSystem>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var randomArray = _randomSystem.RandomArray;
+            Entities
+                .WithNativeDisableParallelForRestriction(randomArray)
+                .ForEach((int nativeThreadIndex, ref Destination destination, in Translation translation) =>
+                {
+                    var distance = math.abs(math.length(destination.value - translation.Value));
+                    if (!(distance <= .1f)) return;
+                    var random = randomArray[nativeThreadIndex];
+                    destination.value.x = random.NextFloat(0, 500);
+                    destination.value.z = random.NextFloat(0, 500);
+                    randomArray[nativeThreadIndex] = random;
+                }).ScheduleParallel();
+        }
+    }
+}
