48b40835e6c45dd8e30df3f2ebe838856a8c734c
Salikh Magomedov <salikh.magomedov@icloud.com>
2022-02-05 18:36:59 +0300

    Detecting Collisions

---
+++ Assets/Scripts/Systems/PersonCollisionSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+namespace Systems
+{
+    public class PersonCollisionSystem : SystemBase
+    {
+        private BuildPhysicsWorld _build;
+        private StepPhysicsWorld _step;
+
+        protected override void OnCreate()
+        {
+            _build = World.GetOrCreateSystem<BuildPhysicsWorld>();
+            _step = World.GetOrCreateSystem<StepPhysicsWorld>();
+        }
+
+        private struct PersonCollisionJob : ITriggerEventsJob
+        {
+            public void Execute(TriggerEvent triggerEvent)
+            {
+                
+            }
+        }
+        
+        protected override void OnUpdate()
+        {
+            Dependency = new PersonCollisionJob().Schedule(_step.Simulation, ref _build.PhysicsWorld, Dependency);
+        }
+    }
+}
